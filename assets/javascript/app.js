e26d47cdae51f192e59ba424a1fd588fd1896faf
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-11-14 12:31:29 -0800

    min away

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,35 +18,84 @@ firebase.initializeApp(firebaseConfig);
 //variable to use firebase database
 var database= firebase.database();
 
+//set to empty string
 var trainName="";
 var destination="";
 var firstTrainTime="";
 var frequencyMin="";
 
-var time= moment().format('LTS')
+//current time for display and later use 
+var time= moment().format('LT')
 $("#tic-toc").html(time);
 
+console.log(time);
 //event listener
 $(".btn").on("click", function(event){
     event.preventDefault();
 
+    //input values
     trainName=$("#train-name").val().trim();
     destination=$("#destination").val().trim();
     firstTrainTime=$("#first-train-time").val().trim();
     frequencyMin=$("#frequency").val().trim();
    
+   
 
-    $("#row").append('<tr>'+ '<td>'+ trainName+ '</td>'+ '<td>'+ destination+ '</td>'+ '<td>'+ frequencyMin+ '</td>'+ '<td>'+ firstTrainTime+ '</td>'+ '<td>'+ " " + '</td>'+'</tr>')
-
+   //push data to firebase 
     database.ref().push({
         trainName,
         destination,
         firstTrainTime,
-        frequencyMin
+        frequencyMin,
+        time,
     })
+
+   
+   
+});
+
+database.ref().on("child_added", function(childSnapshot){
+
+time;
+
+var train= childSnapshot.val().trainName;
+var dest= childSnapshot.val().destination;
+var trainTime= childSnapshot.val().firstTrainTime;
+var frequency= childSnapshot.val().frequencyMin;
+
+console.log("train name" + " "+ train);
+console.log("train destination" + " "+ dest);
+console.log("train time" + " "+ trainTime);
+console.log("train frequency" + " "+ frequency);
+
+//change time so it's like the train already came
+var takeMeBack= moment(trainTime, "hh:mm").subtract(1,"years");
+
+
+console.log ("tmb"+ " "+takeMeBack);
+
+ //putting in minutes
+var difference= moment().diff(moment(takeMeBack), "minutes")
+
+console.log("difference" +" " +difference);
+
+//divide by frequency
+var remainder= difference% frequency;
+
+console.log("remainder:"+ " "+ remainder);
+
+
+var minAway= frequency-remainder;
+
+console.log("min away"+ " "+minAway);
+
+var nextArrival= moment().add(minAway, "minutes");
+
+var display= moment(nextArrival).format("hh:mm");
+    
+$("#row").append('<tr>'+ '<td>'+ trainName+ '</td>'+ '<td>'+ destination+ '</td>'+ '<td>'+ frequencyMin+ '</td>'+ '<td>'+ firstTrainTime+ '</td>'+ '<td>'+ minAway + '</td>'+'</tr>');
+
+
+
+
 })
-
-
-//add rows via user input 
-
-//push rows to firebase database.ref
